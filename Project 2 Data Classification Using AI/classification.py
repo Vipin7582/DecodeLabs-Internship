9c5e818b99e7b9f346ab812a8b5195f0e70ef576
Vipin Singh <vipin758293@gmail.com>
2026-07-20 20:02:26 +0530

    Add files via upload

---
+++ Project 2 Data Classification Using AI/classification.py
@@ -0,0 +1,36 @@
+# Import libraries
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+# Load dataset
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Create model
+model = DecisionTreeClassifier()
+
+# Train model
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+
+# Classification Report
+print("\nClassification Report")
+print(classification_report(y_test, y_pred))
